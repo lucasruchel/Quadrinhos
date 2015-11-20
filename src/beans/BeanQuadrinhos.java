9438adc6ab4f3fe12ea7af21b877faf1dbfdffe0
lucasruchel <lukasruchel@gmail.com>
2015-11-20 09:40:38 -0200

    git ignore add e algumas funcionalidades

---
+++ src/beans/BeanQuadrinhos.java
@@ -0,0 +1,14 @@
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.inject.Named;
+
+/**
+ * Created by wheezy on 20/11/15.
+ */
+
+@Named
+
+@ManagedBean
+public class BeanQuadrinhos {
+}
